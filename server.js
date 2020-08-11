4a03a46634d4a4993b8a339223072d95f0ef2e2d
J0s3-m0v <j0s3.m0v@outlook.es>
2020-08-11 12:21:28 +0100

    update last  heroku

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+//const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname+'/dist/project-angular'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/project-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/project-angular/index.html'));
 });
 
+/*
 app.listen(PORT,()=> {
     console.log("start server at port" + PORT);
 });
-
+*/
