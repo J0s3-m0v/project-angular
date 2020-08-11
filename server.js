fda558e5f9a2146f8dcb57d3b6d68c395f7ede09
J0s3-m0v <j0s3.m0v@outlook.es>
2020-08-11 11:25:14 +0100

    upadate heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const path=require('path');
-const app=express();
+const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static( __dirname + '/dist/project-angular'));
-
-app.get('/*',(req,res)=>{
-    res.sendFile( __dirname + '/dist/project-angular/index.html')
+app.use(express.static(__dirname+'/dist/project-angular'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/project-angular/index.html'));
 });
 
 app.listen(PORT,()=> {
     console.log("start server at port" + PORT);
 });
+
