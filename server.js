b2636d9ad5e19cd6542121b2cc150783c3a62183
J0s3-m0v <j0s3.m0v@outlook.es>
2020-08-13 14:02:25 +0100

    last update config

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
-//const PORT = process.env.PORT || 8080;
-
-app.use(express.static(__dirname+'/dist/project-angular'));
-app.get('/*',function(req,res){
-res.sendFile(path.join(__dirname+'/dist/project-angular/index.html'));
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/project-angular'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/project-angular/index.html'));
 });
+ 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
-/*
-app.listen(PORT,()=> {
-    console.log("start server at port" + PORT);
-});
-*/
